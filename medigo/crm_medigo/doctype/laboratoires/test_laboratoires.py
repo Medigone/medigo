df6c414209b395e862ffe3e274446fdf14956c9d
Amine Melizi <admin@medigo.one>
2023-10-25 19:59:46 +0100

    main

---
+++ medigo/crm_medigo/doctype/laboratoires/test_laboratoires.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Amine Melizi and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLaboratoires(FrappeTestCase):
+	pass
---
+++ medigo/crm_medigo/doctype/visites_services/visites_services.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Amine Melizi and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class VisitesServices(Document):
+	pass
