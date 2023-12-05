61b45c139d1e17ebb0fa14eb57c231164250cd7e
Amine Melizi <admin@medigo.one>
2023-12-05 12:04:20 +0100

    main

--- medigo/crm_medigo/doctype/communes/communes.js
+++ medigo/crm_medigo/doctype/communes/communes.js
@@ -4,5 +4,24 @@
 // frappe.ui.form.on("Communes", {
 // 	refresh(frm) {
 
-// 	},
-// });
+
+frappe.ui.form.on('Communes', {
+    commune: function(frm) {
+        updateNomCompletCommune(frm);
+    },
+
+    wilaya: function(frm) {
+        updateNomCompletCommune(frm);
+    }
+});
+
+function updateNomCompletCommune(frm) {
+    let commune = frm.doc.commune;
+    let wilaya = frm.doc.wilaya;
+
+    if (commune && wilaya) {
+        let nom_complet_commune = commune + " - " + wilaya;
+        frm.set_value('nom_complet_commune', nom_complet_commune);
+    }
+}
+
