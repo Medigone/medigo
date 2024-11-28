1bd022c728b1e293aabd4967897ad95427b41300
Medigone <admin@medigo.one>
2024-11-28 19:35:13 +0100

    New: donnees prescripteur

---
+++ medigo/crm_medigo/doctype/visite_digitale/test_visite_digitale.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Amine Melizi and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestVisiteDigitale(FrappeTestCase):
+	pass
