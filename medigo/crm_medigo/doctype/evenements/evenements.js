92f1ec0a388a8c64f1c8d2eeb53cead77026fa97
Medigone <admin@medigo.one>
2024-12-11 17:44:41 +0100

    Update: ameliorations

--- medigo/crm_medigo/doctype/evenements/evenements.js
+++ medigo/crm_medigo/doctype/evenements/evenements.js
@@ -1,84 +1,48 @@
-// Copyright (c) 2023, Amine Melizi and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Evenements", {
-// 	refresh(frm) {
-
-// 	},
-// });
-frappe.ui.form.on("Evenements", "refresh", function(frm) {
-    frm.fields_dict['etablissement'].grid.get_field('service').get_query = function(doc, cdt, cdn) {
-        var child = locals[cdt][cdn];
-        //console.log(child);
-        return {    
-            filters:[
-                ['etablissement', '=', child.etablissement]
-            ]
-        }
-    }
-});
- 
-// frappe.ui.form.on("Depenses Evenement", {
-//     montant: function(frm, cdt, cdn) {
-//         var d = locals[cdt][cdn];
-//         var total = 0;
-//         frm.doc.depenses.forEach(function(row) { 
-//             total += row.montant || 0; // Ajoute 0 si montant est null ou undefined
-//         });
-//         frm.set_value("total_depenses", total);
-//         refresh_field("total_depenses");
-//     },
-//     depenses_remove: function(frm, cdt, cdn) {
-//         var total = 0;
-//         frm.doc.depenses.forEach(function(row) { 
-//             total += row.montant || 0; // Ajoute 0 si montant est null ou undefined
-//         });
-//         frm.set_value("total_depenses", total);
-//         refresh_field("total_depenses");
-//     }
-// });
-
-frappe.ui.form.on("Depenses Evenement", {
-    montant: function(frm, cdt, cdn) {
-        calculer_total_depenses(frm); // Appel de la fonction pour calculer
-    },
-    depenses_remove: function(frm, cdt, cdn) {
-        calculer_total_depenses(frm); // Appel de la fonction pour calculer après suppression
-    }
-});
-
-function calculer_total_depenses(frm) {
-    var total = 0;
-    if (frm.doc.depenses) {
-        frm.doc.depenses.forEach(function(row) {
-            total += row.montant || 0; // Ajoute 0 si montant est null ou undefined
-        });
-    }
-    frm.set_value("total_depenses", total);
-    refresh_field("total_depenses");
-}
-
-
-frappe.ui.form.on('Evenements', {
+// Gestion des événements pour le Doctype "Evenements"
+frappe.ui.form.on("Evenements", {
     refresh: function(frm) {
         mettre_a_jour_barre_progression(frm);
     },
-    total_depenses: function(frm) {
+    budget_prevu: function(frm) {
+        calculer_ecart_budget(frm);
         mettre_a_jour_barre_progression(frm);
     },
-    budget_prevu: function(frm) {
+    total_depenses: function(frm) {
+        calculer_ecart_budget(frm);
         mettre_a_jour_barre_progression(frm);
+    },
+    participants_presents: function(frm) {
+        calculer_taux_participation(frm);
+    },
+    participants_invites: function(frm) {
+        calculer_taux_participation(frm);
     }
 });
 
+// Gestion des événements pour le Doctype enfant "Depenses Evenement"
+frappe.ui.form.on("Depenses Evenement", {
+    montant: calculer_total_depenses,
+    depenses_remove: calculer_total_depenses
+});
+
+// Fonction pour calculer le total des dépenses
+function calculer_total_depenses(frm) {
+    let total = 0;
+    if (frm.doc.depenses) {
+        frm.doc.depenses.forEach(row => {
+            total += row.montant || 0; // Ajouter 0 si montant est null ou undefined
+        });
+    }
+    frm.set_value("total_depenses", total);
+}
+
+// Fonction pour mettre à jour la barre de progression
 function mettre_a_jour_barre_progression(frm) {
-    // Calculer la progression
     let progression = 0;
     if (frm.doc.budget_prevu && frm.doc.budget_prevu > 0) {
         progression = (frm.doc.total_depenses / frm.doc.budget_prevu) * 100;
     }
 
-    // Définir la couleur en fonction de la progression
     let couleur = '#2a9d8f'; // Vert
     if (progression > 70 && progression <= 100) {
         couleur = '#f4a261'; // Orange entre 70% et 100%
@@ -86,7 +50,6 @@ function mettre_a_jour_barre_progression(frm) {
         couleur = '#e63946'; // Rouge au-delà de 100%
     }
 
-    // Générer le HTML de la barre de progression
     const barre_progression = `
         <div style="width: 100%; margin-top: 0px; margin-bottom: 15px;">
             <label>Progression des Dépenses (${Math.round(progression)}%)</label>
@@ -98,7 +61,25 @@ function mettre_a_jour_barre_progression(frm) {
             </div>
         </div>
     `;
-
-    // Injecter le HTML dans le champ progression_bar
     $(frm.fields_dict.progression_bar.wrapper).html(barre_progression);
+}
+
+// Fonction pour calculer l'écart budgétaire
+function calculer_ecart_budget(frm) {
+    if (frm.doc.budget_prevu && frm.doc.total_depenses) {
+        let ecart = frm.doc.budget_prevu - frm.doc.total_depenses;
+        frm.set_value('ecart_budget', ecart);
+    } else {
+        frm.set_value('ecart_budget', 0);
+    }
+}
+
+// Fonction pour calculer le taux de participation
+function calculer_taux_participation(frm) {
+    if (frm.doc.participants_presents !== undefined && frm.doc.participants_invites > 0) {
+        let taux = (frm.doc.participants_presents / frm.doc.participants_invites) * 100;
+        frm.set_value('taux_participation', taux.toFixed(2)); // Limite à 2 décimales
+    } else {
+        frm.set_value('taux_participation', 0); // Défaut si les données sont insuffisantes
+    }
 }
--- medigo/crm_medigo/doctype/evenements/evenements.py
+++ medigo/crm_medigo/doctype/evenements/evenements.py
@@ -1,13 +1,17 @@
 # Copyright (c) 2023, Amine Melizi and contributors
 # For license information, please see license.txt
 
-# import frappe
 import frappe
 from frappe.model.document import Document
 
 class Evenements(Document):
     def before_save(self):
+        # Calculer le total des dépenses
         self.calculer_total_depenses()
+        # Calculer l'écart budgétaire
+        self.calculer_ecart_budget()
+        # Calculer le taux de participation
+        self.calculer_taux_participation()
 
     def calculer_total_depenses(self):
         """
@@ -19,3 +23,23 @@ class Evenements(Document):
             for depense in self.depenses:
                 total += depense.get("montant", 0)  # Ajoute 0 si montant est None ou inexistant
         self.total_depenses = total
+
+    def calculer_ecart_budget(self):
+        """
+        Calculer l'écart budgétaire en fonction du budget prévu
+        et des dépenses totales.
+        """
+        if self.budget_prevu is not None and self.total_depenses is not None:
+            self.ecart_budget = self.budget_prevu - self.total_depenses
+        else:
+            self.ecart_budget = 0  # Par défaut, 0 si les champs sont vides
+
+    def calculer_taux_participation(self):
+        """
+        Calculer le taux de participation basé sur le nombre de participants présents
+        et le nombre d'invités.
+        """
+        if self.participants_invites and self.participants_invites > 0:
+            self.taux_participation = (self.participants_presents / self.participants_invites) * 100
+        else:
+            self.taux_participation = 0  # Par défaut, 0 si les données sont insuffisantes
