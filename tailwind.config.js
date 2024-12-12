5d59f4069b276d07e1c8ffcd5b237f5690333551
Medigone <admin@medigo.one>
2024-12-12 15:05:14 +0100

    New: Tailwind CSS portal

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./medigo/www/**/*.html",
+    "./medigo/templates/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
